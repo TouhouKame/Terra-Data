c3e2e96bb70bc57af48736e900a68bb6cefcc0ca
TouhouKame <simondcrmc@gmail.com>
2024-07-22 14:26:31 -0700

    first commit

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -5,3 +5,19 @@ export const timeout = function (s) {
     }, s * 1000);
   });
 };
+
+export const correctLongitude = (longitude) =>
+  longitude < -180 ? longitude + 360 : longitude;
+
+export const getJSON = async function (url, errorMsg = 'Something went wrong') {
+  try {
+    const fetchPro = fetch(url);
+    const res = await Promise.race([fetchPro, timeout(10)]);
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
--- scripts/model.js
+++ scripts/model.js
@@ -1,32 +1,79 @@
 // import { TIMEOUT_SEC } from './config';
-import { timeout } from './helpers.js';
+import { timeout, correctLongitude, getJSON } from './helpers.js';
+
+const display = document.querySelector('.display');
+
 let map;
+
+class Country {
+  constructor(country, state, city, id) {
+    this.country = country;
+    this.state = state;
+    this.city = city;
+    this.id = id;
+  }
+
+  view(data) {
+    const html = `
+      <div class="description">
+        <h2 id="country-name">${data}</h2>
+      </div>`;
+    display.insertAdjacentHTML('beforeend', html);
+  }
+
+  createCountryView() {
+    this.view(this.country);
+  }
+  createStateView() {
+    this.view(this.state);
+  }
+  createCityView() {
+    this.view(this.city);
+  }
+}
+
+const countryData = async function (id) {
+  try {
+    getJSON(`https://restcountries.com/v3.1/alpha/${id}`).then((data) =>
+      console.log(data[0])
+    );
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+const displayAllViews = function (country, state, city, id) {
+  const currentCountry = new Country(country, state, city, id);
+  display.innerHTML = '';
+  if (currentCountry.country) currentCountry.createCountryView();
+  if (currentCountry.state) currentCountry.createStateView();
+  if (currentCountry.city) currentCountry.createCityView();
+};
 
 export const getCoords = function (event) {
   const { lat, lng } = event.latlng;
 
   // Calling Geoapify
+
   console.log(lat, lng);
+  // prettier ignore
   getJSON(
-    `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lng}&apiKey=33ef84241f6a4184bd0caa258d7e33c1`
+    `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${correctLongitude(
+      lng
+    )}&apiKey=33ef84241f6a4184bd0caa258d7e33c1`
   )
     .then((data) => {
-      console.log('Data recieved:', data);
+      console.log('Data recieved:', data.features[0].properties);
+      const {
+        country,
+        state,
+        city,
+        country_code: id,
+      } = data.features[0].properties;
+      displayAllViews(country, state, city, id);
+      countryData(id);
     })
     .catch((err) => console.error(`${err} 💥💥`));
-};
-
-export const getJSON = async function (url, errorMsg = 'Something went wrong') {
-  try {
-    const fetchPro = fetch(url);
-    const res = await Promise.race([fetchPro, timeout(10)]);
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-    return data;
-  } catch (err) {
-    throw err;
-  }
 };
 
 export const loadMap = function () {
@@ -37,7 +84,9 @@ export const loadMap = function () {
         const { longitude } = position.coords;
         const coordinates = [latitude, longitude];
         console.log(latitude, longitude);
-        map = L.map('map').setView(coordinates, 6);
+        map = L.map('map', {
+          worldCopyJump: true,
+        }).setView(coordinates, 6);
 
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
           maxZoom: 19,
